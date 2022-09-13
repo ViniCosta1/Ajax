d535b1219dafe3eda503b406b64ec68b2dd0fd2e
ViniCosta <53050163+ViniCosta1@users.noreply.github.com>
2022-09-13 08:45:48 -0300

    API do IBGE (13/09/22)

---
+++ script.js
@@ -0,0 +1,45 @@
+$(document).ready(function (e) {
+    // Ajax para pegar todos os Estados do Brasil
+    $.ajax({
+        type: "get",
+        url: "https://servicodados.ibge.gov.br/api/v1/localidades/estados",
+        data: {orderBy: "nome"},
+        dataType: "json",
+        success: function (response) {
+            // Para cada objeto contendo os estados....
+            $.each(response, function (indexInArray, valueOfElement) { 
+                var option = "<option>"+valueOfElement.sigla+"</option>" // Criar uma variável contendo a tag HTML
+                $("#uf").append(option) // Coloca a variável de HTML dentro da id UF
+            });
+        }
+    });
+
+    // Quando mudar a opção UF...
+    $('#uf').change(function (e) { 
+        e.preventDefault();
+        $("#local").empty();
+        var uf = $("#uf").val();
+
+        // Quando voltar a selecionar "Estados", volta a colocar "cidades" no select de cidades
+        if (uf == 'Estados') {
+            var option = "<option>Cidades</option>"
+            $("#local").append(option)
+            return
+        }        
+
+        // Ajax para pegar todas as cidades de qualquer estado
+        $.ajax({
+            type: "get",
+            url: "https://servicodados.ibge.gov.br/api/v1/localidades/estados/"+uf+"/municipios",
+            data: {orderBy: "nome"},
+            dataType: "json",
+            success: function (response) {
+                // Para cada objeto contendo os cidades....
+                $.each(response, function (indexInArray, valueOfElement) { 
+                    var option = "<option>"+valueOfElement.nome+"</option>" // Criar uma variável contendo a tag HTML
+                    $("#local").append(option) // Coloca a variável de HTML dentro da id local
+                });
+            }
+        });
+    });
+});
